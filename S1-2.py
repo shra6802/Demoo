ec496da720efa837105b1791e3a9447c9e71b49f
shra6802 <shraddhachoudhari04@gmail.com>
2025-11-15 19:26:08 +0530

    s1-2

---
+++ S1-2.py
@@ -0,0 +1,30 @@
+# Import necessary libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import LabelEncoder
+
+
+iris_data = load_iris()
+
+
+df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
+
+
+df['species'] = iris_data.target
+
+
+label_encoder = LabelEncoder()
+df['species'] = label_encoder.fit_transform(df['species'])
+
+print(df.head())
+
+plt.figure(figsize=(8, 6))
+
+sns.scatterplot(x='sepal length (cm)', y='sepal width (cm)', hue='species', data=df, palette='Set2')
+
+plt.title('Scatter Plot of Sepal Length vs Sepal Width')
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
+plt.show()
